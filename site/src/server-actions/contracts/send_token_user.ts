d70b9efb353c22e1416b97e3972720025c92cff0
Roman Njoroge <roman.njoroge@njuguna.com>
2025-05-10 14:11:09 +0300

    feat: Buy on cardano working

--- site/src/server-actions/contracts/send_token_user.ts
+++ site/src/server-actions/contracts/send_token_user.ts
@@ -1,14 +1,24 @@
 "use server"
 
+import { transact } from "@/cardano/transact";
 import { Errors, MyError } from "@/constants/errors";
 import smartContract from "@/contract";
 import { BuyTokenArgs } from "@/types/stocks";
 
-export default async function sendTokensToUser(args: BuyTokenArgs) {
+export async function sendTokensToUser(args: BuyTokenArgs) {
     try {
         await smartContract.buyStockAvalanche(args);
     } catch(err) {
         console.log("Error sending tokens to user", err);
         throw new MyError(Errors.NOT_SEND_TOKENS_USER);
+    }
+}
+
+export async function sendTokensToUserCardano(args:  { tokenId: string, amount: number, receiverAddress: string }) {
+    try {
+        await transact.sendToken(args);
+    } catch(err) {
+        console.error("Error sending tokens");
+        throw new MyError("Could not send tokens to user");
     }
 }
